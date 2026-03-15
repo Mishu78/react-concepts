1fd9758e549e78f96de1c29002e2c4048af83c3e
Mishu78 <meherun2002@gmail.com>
2026-03-15 11:47:28 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+
+
+import './App.css'
+
+function App() {
+  return (
+    <>
+     <Person></Person>
+    </>
+  )
+  
+
+
+
+
+}
+function Person(){
+  return (
+   <p>I am a person </p>
+  )
+}
+
+export default App
