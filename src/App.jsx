370e5eba2091b1a4b0ec828e9975582990315c23
Mishu78 <meherun2002@gmail.com>
2026-03-16 11:22:03 +0600

    device function created and dynamic object created to style div

--- src/App.jsx
+++ src/App.jsx
@@ -6,18 +6,49 @@ function App() {
   return (
     <>
      <Person></Person>
+     <Student></Student>
+     <Device name="laptop" brand="Dell" price="40000"></Device>
+     <Device name="mobile" brand="Redmi" price="15000"></Device>
+     <Device name="TV" brand="Sony" price="60000"></Device>
     </>
   )
-  
-
-
-
 
 }
+
+
 function Person(){
   return (
    <p>I am a person </p>
   )
+}
+
+function Student(){
+  return (
+
+    <div className="student">
+      <p>I am a student</p>
+      <p>Name:</p>
+      <p>Dept:</p>
+      </div>
+  )
+}
+
+function Device(props){
+  console.log(props);
+return (
+  <div style ={
+    {
+      border: '1px solid blue',
+      borderRadius:'20px',
+      padding:'20px'
+    }
+  }>
+    <p>Name:{props.name}</p>
+    <p>Brand:{props.brand}</p>
+    <p>Price:{props.price}</p>
+  </div>
+)
+
 }
 
 export default App
